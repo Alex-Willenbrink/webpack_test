f75a7d2600544c197b25faf0f67e27deb094cf6d
Alex-Willenbrink <awillenbrink@gmail.com>
2017-09-13 21:59:09 -0500

    built production

---
+++ index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./greet.css";
+
+const Greeting = ({ name }) =>
+  <p className="greet">
+    Hello, {name}!
+  </p>;
+
+ReactDOM.render(<Greeting name="Reign" />, document.getElementById("root"));
